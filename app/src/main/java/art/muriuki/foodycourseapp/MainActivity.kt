912bf6e28d89b0332aee2547dda702e5b319655e
rousgidraph <rousgidraph@gmail.com>
2023-10-20 12:41:41 +0300

    :beers: having a data binding challenge

--- app/src/main/java/art/muriuki/foodycourseapp/MainActivity.kt
+++ app/src/main/java/art/muriuki/foodycourseapp/MainActivity.kt
@@ -2,10 +2,30 @@ package art.muriuki.foodycourseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favouriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        // TODO: This is my current headache
+
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
