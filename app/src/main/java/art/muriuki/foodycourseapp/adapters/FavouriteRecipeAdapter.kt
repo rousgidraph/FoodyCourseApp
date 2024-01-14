c2d667b654c511edae86780cc6e637e90b130637
rousgidraph <rousgidraph@gmail.com>
2024-01-14 20:31:26 +0300

    :art: we are able to navigate from the favourites to details

--- app/src/main/java/art/muriuki/foodycourseapp/adapters/FavouriteRecipeAdapter.kt
+++ app/src/main/java/art/muriuki/foodycourseapp/adapters/FavouriteRecipeAdapter.kt
@@ -2,10 +2,13 @@ package art.muriuki.foodycourseapp.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import art.muriuki.foodycourseapp.data.database.entities.FavouritesEntity
 import art.muriuki.foodycourseapp.databinding.FavouriteRecipeRowLayoutBinding
+import art.muriuki.foodycourseapp.models.Result
+import art.muriuki.foodycourseapp.ui.fragments.favourites.FavouriteRecipesFragmentDirections
 import art.muriuki.foodycourseapp.util.RecipesDiffUtil
 
 class FavouriteRecipeAdapter : RecyclerView.Adapter<FavouriteRecipeAdapter.MyViewHolder>() {
@@ -17,7 +20,19 @@ class FavouriteRecipeAdapter : RecyclerView.Adapter<FavouriteRecipeAdapter.MyVie
             fun bind(favouritesEntity: FavouritesEntity) {
                 binding.favouritesEntity = favouritesEntity
                 binding.executePendingBindings()
+
             }
+
+        /**
+         * Single click listener
+         * */
+        fun setOnClick(result: Result,holder: MyViewHolder) {
+            binding.favouriteRecipesRowLayout.setOnClickListener {
+                val action =
+                    FavouriteRecipesFragmentDirections.actionFavouriteRecipesFragmentToDetailsActivity(result)
+                holder.itemView.findNavController().navigate(action)
+            }
+        }
 
         companion object{
             fun from(parent: ViewGroup): MyViewHolder{
@@ -34,9 +49,11 @@ class FavouriteRecipeAdapter : RecyclerView.Adapter<FavouriteRecipeAdapter.MyVie
        return MyViewHolder.from(parent)
     }
 
-    override fun onBindViewHolder(holder: FavouriteRecipeAdapter.MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
        val selectedRecipe = favouritesRecipes[position]
         holder.bind(selectedRecipe)
+        holder.setOnClick(selectedRecipe.result,holder)
+
     }
 
     override fun getItemCount(): Int {
