06aa029dbbbc381d80e9e99f6e2788ddf959b418
rousgidraph <rousgidraph@gmail.com>
2024-01-30 21:46:15 +0300

    added the on long click listener

--- app/src/main/java/art/muriuki/foodycourseapp/adapters/FavouriteRecipeAdapter.kt
+++ app/src/main/java/art/muriuki/foodycourseapp/adapters/FavouriteRecipeAdapter.kt
@@ -1,69 +1,109 @@
 package art.muriuki.foodycourseapp.adapters
 
+import android.view.ActionMode
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import art.muriuki.foodycourseapp.R
 import art.muriuki.foodycourseapp.data.database.entities.FavouritesEntity
 import art.muriuki.foodycourseapp.databinding.FavouriteRecipeRowLayoutBinding
 import art.muriuki.foodycourseapp.models.Result
 import art.muriuki.foodycourseapp.ui.fragments.favourites.FavouriteRecipesFragmentDirections
 import art.muriuki.foodycourseapp.util.RecipesDiffUtil
 
-class FavouriteRecipeAdapter : RecyclerView.Adapter<FavouriteRecipeAdapter.MyViewHolder>() {
+class FavouriteRecipeAdapter(
+    private val requireActivity: FragmentActivity
+) : RecyclerView.Adapter<FavouriteRecipeAdapter.MyViewHolder>(), ActionMode.Callback {
 
     private var favouritesRecipes = emptyList<FavouritesEntity>()
 
-    class MyViewHolder(private val binding: FavouriteRecipeRowLayoutBinding):
+    class MyViewHolder(private val binding: FavouriteRecipeRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
-            fun bind(favouritesEntity: FavouritesEntity) {
-                binding.favouritesEntity = favouritesEntity
-                binding.executePendingBindings()
+        fun bind(favouritesEntity: FavouritesEntity) {
+            binding.favouritesEntity = favouritesEntity
+            binding.executePendingBindings()
 
-            }
+        }
 
         /**
          * Single click listener
          * */
-        fun setOnClick(result: Result,holder: MyViewHolder) {
+        fun setOnClick(result: Result, holder: MyViewHolder) {
             binding.favouriteRecipesRowLayout.setOnClickListener {
                 val action =
-                    FavouriteRecipesFragmentDirections.actionFavouriteRecipesFragmentToDetailsActivity(result)
+                    FavouriteRecipesFragmentDirections.actionFavouriteRecipesFragmentToDetailsActivity(
+                        result
+                    )
                 holder.itemView.findNavController().navigate(action)
             }
         }
 
-        companion object{
-            fun from(parent: ViewGroup): MyViewHolder{
+
+        /**
+         * Long click listener
+         * */
+        fun setOnLongClick( requireActivity: FragmentActivity, adapter :FavouriteRecipeAdapter) {
+            binding.favouriteRecipesRowLayout.setOnLongClickListener {
+                requireActivity.startActionMode(adapter)
+                true
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = FavouriteRecipeRowLayoutBinding.inflate(layoutInflater,parent,false)
+                val binding = FavouriteRecipeRowLayoutBinding.inflate(layoutInflater, parent, false)
                 return MyViewHolder(binding)
             }
         }
-        }
+    }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): FavouriteRecipeAdapter.MyViewHolder {
-       return MyViewHolder.from(parent)
+        return MyViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-       val selectedRecipe = favouritesRecipes[position]
+        val selectedRecipe = favouritesRecipes[position]
         holder.bind(selectedRecipe)
-        holder.setOnClick(selectedRecipe.result,holder)
+        holder.setOnClick(selectedRecipe.result, holder)
+        holder.setOnLongClick(requireActivity,this)
 
     }
 
     override fun getItemCount(): Int {
-       return favouritesRecipes.size
+        return favouritesRecipes.size
     }
 
-    fun setData(newFavouritesEntity: List<FavouritesEntity>){
-        val favouritesEntityRecipesDiffUtil = RecipesDiffUtil(favouritesRecipes,newFavouritesEntity)
+    fun setData(newFavouritesEntity: List<FavouritesEntity>) {
+        val favouritesEntityRecipesDiffUtil =
+            RecipesDiffUtil(favouritesRecipes, newFavouritesEntity)
         val diffUtilResult = DiffUtil.calculateDiff(favouritesEntityRecipesDiffUtil)
         favouritesRecipes = newFavouritesEntity
         diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+    override fun onCreateActionMode(actionMode: ActionMode, menu: Menu?): Boolean {
+        actionMode.menuInflater.inflate(R.menu.favourites_contexual_menu, menu)
+        return true
+    }
+
+    override fun onPrepareActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+       return true
+    }
+
+    override fun onActionItemClicked(actionMode: ActionMode?, item: MenuItem?): Boolean {
+        return true
+    }
+
+    override fun onDestroyActionMode(actionMode: ActionMode?) {
+
     }
 }
